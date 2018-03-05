1c7754be5b98bb598df695ae9b152d8e79278975
Alexander Lucas <32841523+LucasAlexander117825@users.noreply.github.com>
2018-03-05 08:44:54 -0500

    Required Forms
    all forms for Joes Automotive

---
+++ joesAutomotiveCRM/joesAutomotiveCRM/Parts Detail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace joesAutomotiveCRM
+{
+    public partial class Parts_Detail : Form
+    {
+        public Parts_Detail()
+        {
+            InitializeComponent();
+        }
+    }
+}
