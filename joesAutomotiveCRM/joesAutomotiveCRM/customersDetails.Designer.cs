8972279c57249a97df0128724731c35b969f5710
FernsZ <32841555+FernsZ@users.noreply.github.com>
2018-03-06 09:51:54 -0500

    3/6/18 9:51 AM
    Forms and such

---
+++ joesAutomotiveCRM/joesAutomotiveCRM/customersDetails.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace joesAutomotiveCRM
+{
+    partial class customersDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(customersDetails));
+            this.backButton = new System.Windows.Forms.Button();
+            this.vehiclesListBox = new System.Windows.Forms.ListBox();
+            this.customerPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.customerPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(361, 226);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // vehiclesListBox
+            // 
+            this.vehiclesListBox.FormattingEnabled = true;
+            this.vehiclesListBox.Location = new System.Drawing.Point(335, 13);
+            this.vehiclesListBox.Name = "vehiclesListBox";
+            this.vehiclesListBox.Size = new System.Drawing.Size(101, 199);
+            this.vehiclesListBox.TabIndex = 1;
+            // 
+            // customerPicture
+            // 
+            this.customerPicture.Location = new System.Drawing.Point(13, 13);
+            this.customerPicture.Name = "customerPicture";
+            this.customerPicture.Size = new System.Drawing.Size(61, 83);
+            this.customerPicture.TabIndex = 2;
+            this.customerPicture.TabStop = false;
+            // 
+            // customersDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 261);
+            this.Controls.Add(this.customerPicture);
+            this.Controls.Add(this.vehiclesListBox);
+            this.Controls.Add(this.backButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "customersDetails";
+            this.Text = "customerDetails";
+            ((System.ComponentModel.ISupportInitialize)(this.customerPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.ListBox vehiclesListBox;
+        private System.Windows.Forms.PictureBox customerPicture;
+    }
+}
---
+++ joesAutomotiveCRM/joesAutomotiveCRM/customersDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace joesAutomotiveCRM
+{
+    public partial class customersDetails : Form
+    {
+        public customersDetails(string customer)
+        {
+            InitializeComponent();
+            this.Text = customer;
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
